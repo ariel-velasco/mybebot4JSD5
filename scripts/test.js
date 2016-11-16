e86fb4a582fa534cf944bdb9d9a9c82098d1909a
ariel-velasco <ariel.velasco@yahoo.com>
2016-11-15 20:56:54 -0800

    pushing test script2

--- scripts/test.js
+++ scripts/test.js
@@ -1,5 +1,17 @@
+var tuan;
+tuan = ["http://www.yeunhacvang.com/images/article/2013/7/12-7-2013/x42_1.jpg.pagespeed.ic.dcmFU5scVG.jpg"];
+
 module.exports = function(bot) {
  bot.hear(/Kamusta!/, function(res) {
-   return res.send("Hi there!");
+   return res.send("Mabute!");
  });
+};
+
+
+
+  // Random Example
+  //If a user enters 'ship it' we return a random squirrel, which is popular for symbolizing shipping something with engineers
+  return bot.hear(/Maganda Ka?/i, function(msg) {
+    return msg.send(msg.random(tuan));
+  });
 };
